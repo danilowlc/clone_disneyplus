99250c1493956d641166aff9bad99fddd8593d43
danilowlc <danilowlc@gmail.com>
2023-02-12 17:53:30 -0300

    add secao 'FAQ'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i<buttons.length;i++){
         buttons[i].addEventListener('click', function(botao){
@@ -12,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is--active');
             
         }) 
+    }
+
+    for (let i = 0; i<questions.length;i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
     }
 })
 
@@ -28,4 +32,11 @@ function escondeTodasAbas() {
     for(let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
 }
